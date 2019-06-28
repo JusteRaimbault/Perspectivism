c1621f62afc25c0c017743b7bf5cbe4379f889dd
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-06-28 15:54:12 +0000

    test_export

--- Models/QuantEpistemo/run.sh
+++ Models/QuantEpistemo/run.sh
@@ -2,5 +2,6 @@ cd /home/ubuntu/ComplexSystems/Perspectivism/Models/QuantEpistemo
 ./kill.sh
 java -jar torpool.jar 100 9050 --mongo &
 sleep 60s
-./parrun.sh "java -jar bibliodata.jar --citation --mongo persp 5 1 true" 10
+#./parrun.sh "java -jar bibliodata.jar --citation --mongo persp 1 1 true" 5
+./parrun.sh "java -jar bibliodata.jar --citation --mongo persp 1500 0 true" 10
 
